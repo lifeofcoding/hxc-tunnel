cd60397524c7682967bfd4ef14ba064992c94414
LifeOfCoding <LifeOfCoding@gmail.com>
2019-08-10 12:14:16 -0400

    Build #1

---
+++ README.md
@@ -0,0 +1,60 @@
+# socket-tunnel
+
+Tunnel HTTP connections via socket.io streams. Inspired by [localtunnel](https://github.com/localtunnel/localtunnel).
+
+## Blog Post
+
+[Read all about it](https://ericbarch.com/post/sockettunnel/)
+
+## Server Usage
+
+1. Clone this repo and cd into it
+2. docker build -t socket-tunnel .
+3. docker run -d -p 80:3000 --restart=always --name st-server socket-tunnel
+4. Get a domain name (i.e. YOURDOMAIN.com)
+5. Point your domain name's root A record at your server's IP
+6. Point a wildcard (*) A record at your server's IP
+
+## Client CLI Usage
+
+1. Start your http server that you'd like to expose to the public web (in this example we'll assume it's listening on 127.0.0.1:8000)
+2. Clone this repo and cd into it
+3. `npm i`
+4. `node bin/client --server http://YOURDOMAIN.com --subdomain YOURSUBDOMAIN --hostname 127.0.0.1 --port 8000`
+5. Browse to http://YOURSUBDOMAIN.YOURDOMAIN.com to see your local service available on the public internet
+
+## Client API Usage
+
+Assuming a web server running on 127.0.0.1:8000
+
+1. Clone this repo into your project
+2. `npm i`
+3. In your project file, require the socket-tunnel api and call connect():
+
+```JavaScript
+const socketTunnel = require('./socket-tunnel/lib/api');
+
+socketTunnel.connect('http://YOURDOMAIN.com', 'YOURSUBDOMAIN', '8000')
+  .then(console.log)
+  .catch(console.log);
+```
+4. Browse to http://YOURSUBDOMAIN.YOURDOMAIN.com to see your local service available on the public internet
+
+## Client API Parameters
+
+`socketTunnel.connect(remoteServer, desiredSubdomain, localPort, localHostname)` returns a promise which resolves to the requested URL/subdomain.
+
+| Property         | Default     | Description                                        |
+|------------------|-------------|----------------------------------------------------|
+| remoteServer     | n/a         | IP address or hostname of the socket-tunnel server |
+| desiredSubdomain | n/a         | Subdomain to request for this client               |
+| localPort        | n/a         | Local port to tunnel to                            |
+| localHostname    | '127.0.0.1' | Local host to tunnel to                            |
+
+## Credits
+
+Created by Eric Barch. Additional code provided by [these generous contributors](https://github.com/ericbarch/socket-tunnel/graphs/contributors).
+
+## License
+
+This project is licensed under the MIT License - see the LICENSE file for details
---
+++ server.js
@@ -0,0 +1,200 @@
+'use strict';
+
+module.exports = (options) => {
+  // libs
+  const http = require('http');
+  const tldjs = require('tldjs');
+  const ss = require('socket.io-stream');
+  const uuid = require('uuid/v4');
+  const isValidDomain = require('is-valid-domain');
+
+  // association between subdomains and socket.io sockets
+  let socketsBySubdomain = {};
+
+  // bounce incoming http requests to socket.io
+  let server = http.createServer(async (req, res) => {
+    getTunnelClientStreamForReq(req).then((tunnelClientStream) => {
+      const reqBodyChunks = [];
+
+      req.on('error', (err) => {
+        console.error(err.stack);
+      });
+
+      // collect body chunks
+      req.on('data', (bodyChunk) => {
+        reqBodyChunks.push(bodyChunk);
+      });
+
+      // proxy finalized request to tunnel stream
+      req.on('end', () => {
+        // make sure the client didn't die on us
+        if (req.complete) {
+          const reqLine = getReqLineFromReq(req);
+          const headers = getHeadersFromReq(req);
+
+          let reqBody = null;
+          if (reqBodyChunks.length > 0) {
+            reqBody = Buffer.concat(reqBodyChunks);
+          }
+
+          streamResponse(reqLine, headers, reqBody, tunnelClientStream);
+        }
+      });
+    }).catch((subdomainErr) => {
+      res.statusCode = 502;
+      return res.end(subdomainErr.message);
+    });
+  });
+
+  // HTTP upgrades (i.e. websockets) are NOT currently supported because socket.io relies on them
+  // server.on('upgrade', (req, socket, head) => {
+  //   getTunnelClientStreamForReq(req).then((tunnelClientStream) => {
+  //     tunnelClientStream.on('error', () => {
+  //       req.destroy();
+  //       socket.destroy();
+  //       tunnelClientStream.destroy();
+  //     });
+
+  //     // get the upgrade request and send it to the tunnel client
+  //     let messageParts = getHeaderPartsForReq(req);
+
+  //     messageParts.push(''); // Push delimiter
+
+  //     let message = messageParts.join('\r\n');
+  //     tunnelClientStream.write(message);
+
+  //     // pipe data between ingress socket and tunnel client
+  //     tunnelClientStream.pipe(socket).pipe(tunnelClientStream);
+  //   }).catch((subdomainErr) => {
+  //     // if we get an invalid subdomain, this socket is most likely being handled by the root socket.io server
+  //     if (!subdomainErr.message.includes('Invalid subdomain')) {
+  //       socket.end();
+  //     }
+  //   });
+  // });
+
+  function getTunnelClientStreamForReq (req) {
+    return new Promise((resolve, reject) => {
+      // without a hostname, we won't know who the request is for
+      let hostname = req.headers.host;
+      if (!hostname) {
+        return reject(new Error('Invalid hostname'));
+      }
+
+      // make sure we received a subdomain
+      let subdomain = tldjs.getSubdomain(hostname).toLowerCase();
+      if (!subdomain) {
+        return reject(new Error('Invalid subdomain'));
+      }
+
+      // tldjs library return subdomain as all subdomain path from the main domain.
+      // Example:
+      // 1. super.example.com = super
+      // 2. my.super.example.com = my.super
+      // 3. If we are running the tunnel server on a subdomain, we must strip it from the provided hostname
+      if (options.subdomain) {
+        subdomain = subdomain.replace(`.${options.subdomain}`, '');
+      }
+
+      let subdomainSocket = socketsBySubdomain[subdomain];
+      if (!subdomainSocket) {
+        return reject(new Error(`${subdomain} is currently unregistered or offline.`));
+      }
+
+      if (req.connection.tunnelClientStream !== undefined && !req.connection.tunnelClientStream.destroyed && req.connection.subdomain === subdomain) {
+        return resolve(req.connection.tunnelClientStream);
+      }
+
+      let requestGUID = uuid();
+      ss(subdomainSocket).once(requestGUID, (tunnelClientStream) => {
+        req.connection.subdomain = subdomain;
+        req.connection.tunnelClientStream = tunnelClientStream;
+
+        // Pipe all data from tunnel stream to requesting connection
+        tunnelClientStream.pipe(req.connection);
+
+        resolve(tunnelClientStream);
+      });
+
+      subdomainSocket.emit('incomingClient', requestGUID);
+    });
+  }
+
+  function getReqLineFromReq (req) {
+    return `${req.method} ${req.url} HTTP/${req.httpVersion}`;
+  }
+
+  function getHeadersFromReq (req) {
+    const headers = [];
+
+    for (let i = 0; i < (req.rawHeaders.length - 1); i += 2) {
+      headers.push(req.rawHeaders[i] + ': ' + req.rawHeaders[i + 1]);
+    }
+
+    return headers;
+  }
+
+  function streamResponse (reqLine, headers, reqBody, tunnelClientStream) {
+    tunnelClientStream.write(reqLine);
+    tunnelClientStream.write('\r\n');
+    tunnelClientStream.write(headers.join('\r\n'));
+    tunnelClientStream.write('\r\n\r\n');
+    if (reqBody) {
+      tunnelClientStream.write(reqBody);
+    }
+  }
+
+  // socket.io instance
+  let io = require('socket.io')(server);
+  io.on('connection', (socket) => {
+    socket.on('createTunnel', (requestedName, responseCb) => {
+      if (socket.requestedName) {
+        // tunnel has already been created
+        return;
+      }
+
+      // domains are case insensitive
+      let reqNameNormalized = requestedName.toString().toLowerCase().replace(/[^0-9a-z-]/g, '');
+
+      // make sure the client is requesting a valid subdomain
+      if (reqNameNormalized.length === 0 || !isValidDomain(`${reqNameNormalized}.example.com`)) {
+        console.log(new Date() + ': ' + reqNameNormalized + ' -- bad subdomain. disconnecting client.');
+        if (responseCb) {
+          responseCb('bad subdomain');
+        }
+        return socket.disconnect();
+      }
+
+      // make sure someone else hasn't claimed this subdomain
+      if (socketsBySubdomain[reqNameNormalized]) {
+        console.log(new Date() + ': ' + reqNameNormalized + ' requested but already claimed. disconnecting client.');
+        if (responseCb) {
+          responseCb('subdomain already claimed');
+        }
+        return socket.disconnect();
+      }
+
+      // store a reference to this socket by the subdomain claimed
+      socketsBySubdomain[reqNameNormalized] = socket;
+      socket.requestedName = reqNameNormalized;
+      console.log(new Date() + ': ' + reqNameNormalized + ' registered successfully');
+
+      if (responseCb) {
+        responseCb(null);
+      }
+    });
+
+    // when a client disconnects, we need to remove their association
+    socket.on('disconnect', () => {
+      if (socket.requestedName) {
+        delete socketsBySubdomain[socket.requestedName];
+        console.log(new Date() + ': ' + socket.requestedName + ' unregistered');
+      }
+    });
+  });
+
+  // http server
+  server.listen(options.port, options.hostname);
+
+  console.log(`${new Date()}: socket-tunnel server started on ${options.hostname}:${options.port}`);
+};
